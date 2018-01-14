be268bfc113d39e8e57f5b937f7618644d166b9d
Evan Wheeler <anon0mys87@gmail.com>
2018-01-14 13:24:11 -0700

    Changed guess logic and fixed minor formatting issues.

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,14 +1,13 @@
 class GuessingGame
 
-  @@game_tracker = {}
+  @@game_tracker = []
 
-    def initialize(x)
-        @game_num = x
+    def initialize()
         @game_info = {
-        guess_count: 0,
-        cheat: false,
-        number: rand(100).to_i,
-        hint: false
+          guess_count: 0,
+          cheat: false,
+          number: rand(100).to_i,
+          hint: false
         }
 
         puts "I have generated a random number for you to guess!"
@@ -22,17 +21,17 @@ class GuessingGame
         print "What is your guess? > "
         guess = gets.chomp
 
-        if !(guess.scan(/\D/).empty?) && (guess =~ /cheat/ || guess == "c")
+        if guess =~ /cheat/ || guess == "c" #If it matches cheat then do cheat
           @game_info[:cheat] = true
           puts "That's cheating! The number is #{@game_info[:number]}. Guess again."
           guess()
-        elsif !(guess.scan(/\D/).empty?) && !(guess == "cheat" || guess == "c")
-          puts "Please guess a number."
-          guess()
-        else
+        elsif guess.scan(/\D/).empty? #If it is all digits run test, move please guess to catch-all
           guess = guess.to_i
           @game_info[:guess_count] += 1
           test(guess)
+        else
+          puts "Please guess a number."
+          guess()
         end
     end
 
@@ -44,9 +43,6 @@ class GuessingGame
           guess()
         elsif guess < @game_info[:number]
           puts "You guessed too low! Guess again."
-          guess()
-        else
-          puts "Not sure how you got here..."
           guess()
         end
     end
@@ -61,29 +57,30 @@ class GuessingGame
         else
             puts "That's a lot of guesses..."
         end
-        @@game_tracker[@game_num] = @game_info
+        @@game_tracker << @game_info
         again()
     end
 
-    def hint()
+    def hint(number = @game_info[:number])
       @game_info[:hint] = true
-        if @number.to_i.even?
-            puts "The number is even!"
-            guess()
-        else
-            puts "The number is odd!"
-            guess()
-        end
+
+      if number.even?
+          puts "The number is even!"
+          guess()
+      else
+          puts "The number is odd!"
+          guess()
+      end
     end
 
     def again()
         print "Would you like to play again? Yes/No > "
         again = gets.chomp
         if again =~ /yes/i
-            game = GuessingGame.new(@game_num + 1)
+            game = GuessingGame.new()
             game.guess()
         else
-            puts "You played #{@game_num} games!"
+            puts "You played #{@@game_tracker.length} games!"
             print "Would you like to see how you did? Yes/No > "
             track = gets.chomp
             tracker(track)
@@ -92,8 +89,8 @@ class GuessingGame
 
     def tracker(track)
         if track =~ /yes/i
-          @@game_tracker.each do |game, game_info|
-            puts "Game #{game}: #{game_info}"
+          @@game_tracker.each_with_index do |game_info, index|
+            puts "Game #{index + 1}: #{game_info}"
           end
           puts "Goodbye!"
           exit(0)
@@ -105,5 +102,5 @@ class GuessingGame
 
 end
 
-game = GuessingGame.new(1)
+game = GuessingGame.new()
 game.guess()
